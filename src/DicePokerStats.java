da610412bd07b23c85aa6f9692f89b3f3b24b32c
SethHikari@gmail.com <SethHikari@gmail.com@025c37ad-ea19-689f-147e-afa861d2430e>
2011-06-06 06:28:51 +0000

    Changed roll List to be an ArrayList
    Added 2 pair

--- src/DicePokerStats.java
+++ src/DicePokerStats.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class DicePokerStats {
 	private static long mTotalRolls;
+	private static int mRepeatedDice;
 
 	/**
 	 * This program rolls every possible 5 dice combination the prints the stats
@@ -19,6 +20,7 @@ public class DicePokerStats {
 		statsOfTriples(allRolls);
 		statsOf4OfAKind(allRolls);
 		statsOf5OfAKind(allRolls);
+		statsOf2Pairs(allRolls);
 	}
 
 	/**
@@ -40,8 +42,12 @@ public class DicePokerStats {
 						for (int dice5 = 1; dice5 <= 6; dice5++) {
 							// add to the list a new double array of all the
 							// dice so far
-							List<Integer> roll = Arrays.asList(dice1, dice2, dice3,
-									dice4, dice5);
+							List<Integer> roll = new ArrayList<Integer>();
+							roll.add(dice1);
+							roll.add(dice2);
+							roll.add(dice3);
+							roll.add(dice4);
+							roll.add(dice5);
 							rollList.add(roll);
 							countPossibleRolls++;
 						}
@@ -86,7 +92,7 @@ public class DicePokerStats {
 		System.out.println("So the chance of rolling triples is: "
 				+ ((double) count / (double) mTotalRolls) * 100d + " %");
 	}
-	
+
 	private static void statsOf4OfAKind(List<List<Integer>> allRolls) {
 		long count = 0;
 		for (List<Integer> roll : allRolls) {
@@ -94,12 +100,13 @@ public class DicePokerStats {
 				count++;
 			}
 		}
-		System.out.println("The amount of 4 of a kind in all the rolls possible: "
-				+ count);
+		System.out
+				.println("The amount of 4 of a kind in all the rolls possible: "
+						+ count);
 		System.out.println("So the chance of rolling 4 of a kind is: "
 				+ ((double) count / (double) mTotalRolls) * 100d + " %");
 	}
-	
+
 	private static void statsOf5OfAKind(List<List<Integer>> allRolls) {
 		long count = 0;
 		for (List<Integer> roll : allRolls) {
@@ -107,25 +114,45 @@ public class DicePokerStats {
 				count++;
 			}
 		}
-		System.out.println("The amount of 5 of a kind in all the rolls possible: "
-				+ count);
+		System.out
+				.println("The amount of 5 of a kind in all the rolls possible: "
+						+ count);
 		System.out.println("So the chance of rolling 5 of a kind is: "
 				+ ((double) count / (double) mTotalRolls) * 100d + " %");
 	}
-	
-	 
-	 private static boolean hasMultiplesOf(List<Integer> roll,int amount) {
-		    for (int i = 0; i < roll.size(); i++){
-		        int duplicates = 0;
 
-		        for (int j = i+1; j < roll.size(); j++){
-		            if (roll.get(i) == roll.get(j)) {
-		                duplicates++;
-		                if (duplicates >= amount - 1) return true;
-		            }
-		        }
-		    }
-		    return false;
+	private static void statsOf2Pairs(List<List<Integer>> allRolls) {
+		long count = 0;
+		for (List<Integer> roll : allRolls) {
+			if (hasMultiplesOf(roll, 2)) {
+				roll.removeAll(Arrays.asList(mRepeatedDice));
+				if(hasMultiplesOf(roll, 2)) {
+					count++;
+				}
+			}
 		}
+		System.out
+				.println("The amount of 2 pairs in all the rolls possible: "
+						+ count);
+		System.out.println("So the chance of rolling 2 pairs is: "
+				+ ((double) count / (double) mTotalRolls) * 100d + " %");
+	}
+
+	private static boolean hasMultiplesOf(List<Integer> roll, int amount) {
+		for (int i = 0; i < roll.size(); i++) {
+			int duplicates = 0;
+
+			for (int j = i + 1; j < roll.size(); j++) {
+				if (roll.get(i) == roll.get(j)) {
+					duplicates++;
+					if (duplicates >= amount - 1) {
+						mRepeatedDice = roll.get(i);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 
 }
